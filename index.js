1f19103668d3a9061e61b3dca48bb539aec54aa4
Maryna <marinakukusua@gmail.com>
2025-10-18 18:04:35 +0300

    Comparison string

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-let num = Number(prompt("Enter a number from 0 to 7:"));
+let num = prompt("Enter a number from 0 to 7:");
 console.log(num);
   switch (num) {
-    case 0:
+    case '0':
       console.log("---");
       break;
-    case 1:
+    case '1':
       console.log("--x");
       break;
-    case 2:
+    case '2':
       console.log("-w-");
       break;
-    case 3:
+    case '3':
       console.log("-wx");
       break;
-    case 4:
+    case '4':
       console.log("r--");
       break;
-    case 5:
+    case '5':
       console.log("r-x");
       break;
-    case 6:
+    case '6':
       console.log("rw-");
       break;
-    case 7:
+    case '7':
       console.log("rwx");
       break;
     default: console.log("Entered wrong data");
